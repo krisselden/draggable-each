10e41fe72ed06ce33fa624cc315c81b512c59c98
Luke Melia <luke@lukemelia.com>
2014-08-23 12:51:01 -0400

    Simplify examples

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -4,9 +4,6 @@ export default Ember.Controller.extend({
   lists: [1,2],
   otherList: [],
   actions: {
-    save: function(model) {
-      window.alert('did save: ' + model.name);
-    },
     itemWasMoved: function(model, from, to) {
       Ember.Logger.info('itemWasMoved', model, from, '->', to);
     }
