5acda219937e3c4b51dbc7c8145bd3d1e8f5ad5a
Luke Melia <luke@lukemelia.com>
2014-08-30 22:24:40 -0400

    Add grunt-bump to ease releasing

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-bump');
+
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
+    },
+  });
+};
